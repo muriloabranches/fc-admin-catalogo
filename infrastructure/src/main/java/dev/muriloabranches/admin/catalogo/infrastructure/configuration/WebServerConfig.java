169f19d62a18e6e37e5836f76d2028ce0813d8a8
MuriloAbranches <muriloads@hotmail.com>
2023-08-01 22:49:05 -0300

    Springboot and configurations

---
+++ infrastructure/src/main/java/dev/muriloabranches/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package dev.muriloabranches.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("dev.muriloabranches.admin.catalogo")
+public class WebServerConfig {
+}
